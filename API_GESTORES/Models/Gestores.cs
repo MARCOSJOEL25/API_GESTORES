b54d5222b7eedafc0ab75f326969507d5c90b8e5
marcosjoel369 <20209155@itla.edu.do>
2022-09-06 20:01:54 -0400

    Add project files.

---
+++ API_GESTORES/Models/Gestores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_GESTORES.Models
+{
+    public class Gestores
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int lanzamiento { get; set; }
+        public string desarrolador { get; set; }
+    }
+}
---
+++ API_GESTORES/contextDB/contextDB.cs
@@ -0,0 +1,19 @@
+﻿using API_GESTORES.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_GESTORES.contextDB
+{
+    public class contextDB : DbContext
+    {
+        public contextDB(DbContextOptions<contextDB> options): base(options)
+        {
+
+        }
+
+        public DbSet<Gestores>
+    }
+}
